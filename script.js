0b8f7479bb207b240ab8c131d03d8e97d33f59bd
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-03-08 19:28:05 -0800

    testing out querySelectors

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const removeDownArrow = () => {
-    const downArrow = document.getElementById('arrowDown');
-    const upArrow = document.getElementById('arrowUp');
     const featuresNav = document.getElementById('features');
-    console.log(featuresNav);
-    // for (const arrow of upArrow) {
+    const downArrow = document.querySelector('#features .arrowDown');
+    const upArrow = document.querySelector('#features .arrowUp');
+    
+    const companyNav = document.getElementById('company');
+    const arrowDown = document.querySelector('#company .arrowDown');
+    const arrowUp = document.querySelector('#company .arrowUp');
+
         featuresNav.addEventListener('mouseover', () => {
             downArrow.style.display = 'none';
             upArrow.style.display = 'block';
@@ -12,6 +15,14 @@ const removeDownArrow = () => {
             downArrow.style.display = 'block';
             upArrow.style.display = 'none';
         })
-    // }
+
+        companyNav.addEventListener('mouseover', () => {
+            arrowDown.style.display = 'none';
+            arrowUp.style.display = 'block';
+        })
+        companyNav.addEventListener('mouseout', () => {
+            arrowDown.style.display = 'block';
+            arrowUp.style.display = 'none';
+        })
 }
 removeDownArrow()
