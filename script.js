12aa75e089e65ab8e52ea5b7cab8a810eecd2be9
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-03-08 19:02:30 -0800

    event listeners on feature an comp

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const removeDownArrow = () => {
     const downArrow = document.getElementById('arrowDown');
     const upArrow = document.getElementById('arrowUp');
     const featuresNav = document.getElementById('features');
-
-    featuresNav.addEventListener('mouseover', () => {
-        downArrow.style.display = 'none';
-        upArrow.style.display = 'block';
-    })
-    featuresNav.addEventListener('mouseout', () => {
-        downArrow.style.display = 'block';
-        upArrow.style.display = 'none';
-    })
+    console.log(featuresNav);
+    // for (const arrow of upArrow) {
+        featuresNav.addEventListener('mouseover', () => {
+            downArrow.style.display = 'none';
+            upArrow.style.display = 'block';
+        })
+        featuresNav.addEventListener('mouseout', () => {
+            downArrow.style.display = 'block';
+            upArrow.style.display = 'none';
+        })
+    // }
 }
 removeDownArrow()
