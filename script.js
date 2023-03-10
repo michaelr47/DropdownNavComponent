d0cccd4275343df632d113c4c573ca524fb2d012
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-03-09 19:25:47 -0800

    utilized onmouseover and onmouseout for imgs

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-const removeDownArrow = () => {
-    const featuresNav = document.getElementById('features');
-    const downArrow = document.querySelector('#features .arrowDown');
-    const upArrow = document.querySelector('#features .arrowUp');
-    
-    const companyNav = document.getElementById('company');
-    const arrowDown = document.querySelector('#company .arrowDown');
-    const arrowUp = document.querySelector('#company .arrowUp');
-
-        featuresNav.addEventListener('mouseover', () => {
-            downArrow.style.display = 'none';
-            upArrow.style.display = 'block';
-        })
-        featuresNav.addEventListener('mouseout', () => {
-            downArrow.style.display = 'block';
-            upArrow.style.display = 'none';
-        })
-
-        companyNav.addEventListener('mouseover', () => {
-            arrowDown.style.display = 'none';
-            arrowUp.style.display = 'block';
-        })
-        companyNav.addEventListener('mouseout', () => {
-            arrowDown.style.display = 'block';
-            arrowUp.style.display = 'none';
-        })
+const getArrowUpImg = () => {
+    document.querySelectorAll('.arrowDown').src = './images/icon-arrow-up.svg';
 }
-removeDownArrow()
+const getArrowDownImg = () => {
+    document.querySelectorAll('.arrowDown').src = './images/icon-arrow-down.svg';
+}
