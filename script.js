9b1f9423f2c5d608ba2b6bf812a65dd3b8e5429a
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-03-14 18:59:27 -0700

    feature and company dropdown down

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const getArrowUpImg1 = () => {
     let img1 = document.querySelector('#features .arrowDown');
+    let dropdownFeatures = document.getElementById('dropdownFeatures');
+
     img1.src = './images/icon-arrow-up.svg';
+    dropdownFeatures.style.display = 'block'
 }
 
 const getArrowUpImg2 = () => {
     let img2 = document.querySelector('#company .arrowDown');
+    let dropdownCompany = document.getElementById('dropdownCompany');
+
     img2.src = './images/icon-arrow-up.svg';
+    dropdownCompany.style.display = 'block';
 }
 
 const getArrowDownImg1 = () => {
     let img1 = document.querySelector('#features .arrowDown');
+    let dropdownFeatures = document.getElementById('dropdownFeatures');
+
     img1.src = './images/icon-arrow-down.svg';
+    dropdownFeatures.style.display = 'none'
 }
 
 const getArrowDownImg2 = () => {
     let img2 = document.querySelector('#company .arrowDown');
+    let dropdownCompany = document.getElementById('dropdownCompany');
+
     img2.src = './images/icon-arrow-down.svg';
+    dropdownCompany.style.display = 'none';
 }
