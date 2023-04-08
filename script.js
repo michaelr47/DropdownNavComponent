a4860b8c6e9af9212b2194be6e1a804ac596d91e
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-04-07 19:01:30 -0700

    transition on nav

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ const arrowDownComp = document.querySelector('#company .arrowDown');
 const getArrowUpImg1 = () => {
     arrowDownFeat.src = './images/icon-arrow-up.svg';
     dropdownFeatures.style.display = 'block'
-
-      window.addEventListener('click', (e) => {
-        if (e.target == window) {
-            console.log('clicked window');
-            dropdownFeatures.style.display = 'none';
-            // nav.style.display = 'none';
-            // hamburgerMenu.style.display = 'block';
-        }
-    })
-
 }
  
 const getArrowUpImg2 = () => {
@@ -37,8 +27,6 @@ const getArrowDownImg2 = () => {
 const hamburgerMenu = document.getElementById('hamburgerIcon');
 
 const displayNavMobile = () => {
-    
-   
     hamburgerMenu.style.cursor = 'pointer';
     const nav = document.getElementById('mobileNav');
     const closeMenu = document.querySelector('.closeNavX');
@@ -77,35 +65,31 @@ const changeArrowDirecttion = () => {
     const featuresEl = document.getElementById('featuresMobileNav');
     const companyEl = document.getElementById('companyMobileNav');
 
-        featuresEl.addEventListener(('click'), () => {
-            if (arrowUp[0].style.display === 'none') {
-                arrowUp[0].style.display = 'inline-block';
-                featMob.style.display = 'block'
-                arrowDown[0].style.display = 'none'
-            } 
-            else {
-                arrowUp[0].style.display = 'none';
-                arrowDown[0].style.display = 'inline-block'
-                featMob.style.display = 'none'
-
-            }
-
-        })
-
+    featuresEl.addEventListener(('click'), () => {
+        if (arrowUp[0].style.display === 'none') {
+            arrowUp[0].style.display = 'inline-block';
+            featMob.style.display = 'block'
+            arrowDown[0].style.display = 'none'
+        } 
+        else {
+            arrowUp[0].style.display = 'none';
+            arrowDown[0].style.display = 'inline-block'
+            featMob.style.display = 'none'
+        }
+    })
     //company arrows
-    
-        companyEl.addEventListener('click', () => {
-            if (arrowUp[1].style.display === 'none') {
-                arrowUp[1].style.display = 'inline-block'; 
-                compMob.style.display = 'block';
-                arrowDown[1].style.display = 'none'; 
-            } else {
-                arrowUp[1].style.display = 'none';
-                arrowDown[1].style.display = 'inline-block';
-                compMob.style.display = 'none';
-
-            }
-        })
+    companyEl.addEventListener('click', () => {
+        if (arrowUp[1].style.display === 'none') {
+            arrowUp[1].style.display = 'inline-block'; 
+            compMob.style.display = 'block';
+            arrowDown[1].style.display = 'none'; 
+        } 
+        else {
+            arrowUp[1].style.display = 'none';
+            arrowDown[1].style.display = 'inline-block';
+            compMob.style.display = 'none';
+        }
+    })
     
 }
 
